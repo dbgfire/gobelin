d90be7d6f1357f4e03bb46a3190a92ad8bd5d4c9
Unknown <lmouelo@yahoo.com>
2018-01-09 16:09:49 +0100

    Auto stash before merge of "master" and "origin/master"

--- Gobelin/src/Main.java
+++ Gobelin/src/Main.java
@@ -1,9 +1,14 @@
+<<<<<<< Updated upstream
 import javax.swing.*;
 
+=======
+import javax.swing.JFrame;
+>>>>>>> Stashed changes
 public class Main {
 
     public static void main(String[] args) {
 
+<<<<<<< Updated upstream
         JFrame fenetre = new JFrame();
         //Définit un titre pour notre fenêtre
         fenetre.setTitle("Goblin");
@@ -15,5 +20,17 @@ public class Main {
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Et enfin, la rendre visible
         fenetre.setVisible(true);
+=======
+        System.out.println("Hello World!");
+        JFrame fenetre = new JFrame();
+        fenetre.setVisible(true);
+        //Définit un titre pour notre fenêtre
+        fenetre.setTitle("goblin");
+        //Définit sa taille : 400 pixels de large et 100 pixels de haut
+        fenetre.setSize(3000, 2000);
+        //Nous demandons maintenant à notre objet de se positionner au centre
+        fenetre.setLocationRelativeTo(null);
+
+>>>>>>> Stashed changes
     }
 }
